68dd7e9c5c722122712e0432520d086decd14b33
Loocien <nicolassilveiracorrea@gmail.com>
2023-11-14 19:44:52 -0300

    Good solution

---
+++ src/main/java/com/nic/model/exceptions/DomainException.java
@@ -0,0 +1,9 @@
+package com.nic.model.exceptions;
+
+public class DomainException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public DomainException(String msg) {
+        super(msg);
+    }
+}
